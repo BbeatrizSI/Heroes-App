ccf860f8adce7b993e8c81d00ee97e9e618b10b0
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-01 21:28:59 +0100

    add animations

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroByPublisher';
 import HeroCard from './HeroCard';
 
 const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher(publisher);
+    const heroes = useMemo( () => getHeroesByPublisher(publisher), [publisher]);
 
     return (
-        <div className="card-columns">
+        <div className="card-columns animate__animated animate__fadeIn">
             {
                 heroes.map(hero => (
                     <HeroCard key= {hero.id} {...hero} />
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getHeroesById } from '../../selectors/getHeroById';
 
-const HeroScreen = () => {
+const HeroScreen = ({ history }) => {
+
+    const {heroId} = useParams();
+
+    const hero = useMemo( () => getHeroesById( heroId ), [ heroId ])
+
+    if (!hero) {
+        return <Redirect to="/" />
+    }
+
+    const handleReturn = () => {
+        if(history.length <= 2) {
+            history.push('/');
+        } else {
+        history.goBack();
+        }
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={`../assets/heroes/${heroId}.jpg`} className="img-thumbnail animate__animated animate__fadeInLeft" alt={superhero} />
+            </div>
+            <div className="col-8 animate__animated animate__fadeIn">
+                <h3>{ superhero }</h3>
+            
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego:</b> { alter_ego }</li>
+                    <li className="list-group-item"><b>Publisher:</b> { publisher }</li>
+                    <li className="list-group-item"><b>First appearance:</b> { first_appearance }</li>
+                </ul>
+                <h5>Characters</h5>
+                <p>{ characters }</p>
+
+                <button className="btn btn-outline-info" onClick={handleReturn}>
+                Return
+                </button>
+            </div>
         </div>
     );
 };

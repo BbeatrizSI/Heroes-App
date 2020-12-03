d88e8102320d6dd5934aa92925674257c34ac3df
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-03 21:02:52 +0100

    add search component but not finished yet

---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { heroes } from '../../data/heroes';
+import { useForm } from '../../hooks/useForm';
+import HeroCard from '../heroes/HeroCard';
+
+const SearchScreen = () => {
+    const heroesFiltered = heroes;
+
+    const [ formValues, handleInputChange ] = useForm ({searchText: ''});
+
+    const {searchText} = formValues;
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+    }
+
+    return (
+        <div>
+            <h1>Search Screen</h1>
+            <hr/>
+            <div className="row">
+                <div className="col-5">
+                    <h4>Search form</h4>
+                    <hr/>
+                    <form onSubmit={handleSearch}>
+                        <input type="text" placeholder="Find your hero" className="form-control" name="searchText" value={ searchText } onChange={ handleInputChange } autoComplete="off"/>
+                        <button type="submit" className="btn m-1 btn-block btn-outline-primary">Search</button>
+                    </form>
+                </div>
+                <div className="col-7">
+                    <h4>Results</h4>
+                    <hr/>
+                    {
+                        heroesFiltered.map( hero => (
+                            <HeroCard key={hero.id} {...hero} />
+                        ))
+                    }
+
+                </div>  
+            </div>
+        </div>
+    );
+};
+
+export default SearchScreen;
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import DcScreen from '../components/dc/DcScreen';
 import HeroScreen from '../components/heroes/HeroScreen';
 import MarvelScreen from '../components/marvel/MarvelScreen';
+import SearchScreen from '../components/search/SearchScreen';
 import { Navbar } from '../components/ui/NavBar';
 
 const DashboardRoutes = () => {
@@ -14,6 +15,8 @@ const DashboardRoutes = () => {
                     <Route exact path="/marvel" component={ MarvelScreen } />
                     <Route exact path="/hero/:heroId" component={ HeroScreen } />
                     <Route exact path="/dc" component={ DcScreen } />
+                    <Route exact path="/search" component={ SearchScreen } />
+
                     <Redirect to="/marvel" />
                 </Switch>
             </div>
